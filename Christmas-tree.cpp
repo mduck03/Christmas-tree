0233715a6f8ac85d113c2d60b37ca584219d27d6
mduck03 <61601954+mduck03@users.noreply.github.com>
2020-12-07 20:46:31 +0100

    fully working compilated program

--- Christmas-tree.cpp
+++ Christmas-tree.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main()
 {
     int HowManyLine;
-    cout >> "How many line u will draw?";
-    cin << HowManyLine;//need to bee odd and positive number
+    cout << "How many line u will draw: ";
+    cin >> HowManyLine;//need to bee odd and positive number
     int ActualLine = HowManyLine;
     for (int i = 0; i < HowManyLine; i++) {
-        for (int x = 0; x < ActualLine-1; x++) {
+        for (int x = 0; x < ActualLine - 1; x++) {
             cout << "o";
         }
-        for (int x = HowManyLine+1; x > ActualLine ; x--) {
+        for (int x = HowManyLine + 1; x > ActualLine; x--) {
             cout << "x";
         }
         for (int x = HowManyLine; x > ActualLine; x--) {
@@ -22,5 +22,9 @@ int main()
         }
         ActualLine--;
         cout << endl;
+        
     }
+    int wait;
+    cout << "type enything to stop";
+    cin >> wait;
 }
